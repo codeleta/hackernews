a5ffa486633c731402df9c868844f461cd5903be
yademidenko <admin@papara.ru>
2020-07-26 22:22:11 +0300

    feat: add client for hackernews

---
+++ server/apps/main/logic/client_hackernews.py
@@ -0,0 +1,90 @@
+import dataclasses
+import logging
+import typing
+
+import bs4
+import requests
+import typing_extensions
+from requests import adapters
+from urllib3.util import retry
+
+logger = logging.getLogger(__name__)
+
+HACKERNEWS_HOST: typing_extensions.Final = 'https://news.ycombinator.com/'
+
+
+@dataclasses.dataclass
+class Post(object):
+    """Post from hackernews site."""
+    url: str
+    title: str
+
+
+class HackernewsError(Exception):
+    """Common exception for hackernews client."""
+
+
+class HackernewsRequestError(HackernewsError):
+    """Raises when requests.get failed."""
+
+
+class HackernewsParseError(HackernewsError):
+    """Raises when posts not found."""
+
+
+def _requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry_policy = retry.Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = adapters.HTTPAdapter(max_retries=retry_policy)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+
+def _get_index_page_text() -> str:
+    hackernews_index_page = _requests_retry_session().get(HACKERNEWS_HOST)
+    try:
+        hackernews_index_page.raise_for_status()
+    except requests.HTTPError as exc:
+        logger.error(
+            'Request to %s failed with status %s: %s',
+            HACKERNEWS_HOST,
+            hackernews_index_page.status_code,
+            exc,
+        )
+        raise HackernewsRequestError(str(exc))
+    return hackernews_index_page.text
+
+
+def _parse_posts(page_html: str) -> typing.List[Post]:
+    parsed_html = bs4.BeautifulSoup(page_html, 'html.parser')
+    posts: typing.List[Post] = []
+    post_links = parsed_html.findAll('a', {'class': 'storylink'})
+    if not post_links:
+        error_message = 'posts not found'
+        logger.error(error_message)
+        raise HackernewsParseError(error_message)
+    for post_link in post_links:
+        posts.append(
+            Post(
+                url=post_link.get('href'),
+                title=post_link.text,
+            ),
+        )
+    return posts
+
+
+def get_posts() -> typing.List[Post]:
+    """Get posts from hackernews site."""
+    return _parse_posts(_get_index_page_text())
---
+++ tests/test_apps/test_main/test_logic/test_client_hackernews.py
@@ -0,0 +1,38 @@
+import pytest
+import requests_mock
+
+from server.apps.main.logic import client_hackernews
+
+
+def test_get_posts_success():
+    post_url = 'http://example.com'
+    post_title = 'Title'
+    with requests_mock.Mocker() as mocked_requests:
+        mocked_requests.get(
+            client_hackernews.HACKERNEWS_HOST,
+            text=f'<a href="{post_url}" class="storylink">{post_title}</a>',
+        )
+        posts = client_hackernews.get_posts()
+    expected_posts = [client_hackernews.Post(url=post_url, title=post_title)]
+    assert posts == expected_posts
+
+
+def test_get_posts_500():
+    with requests_mock.Mocker() as mocked_requests:
+        mocked_requests.get(
+            client_hackernews.HACKERNEWS_HOST,
+            status_code=500,
+            text='internal error',
+        )
+        with pytest.raises(client_hackernews.HackernewsRequestError):
+            client_hackernews.get_posts()
+
+
+def test_get_posts_not_found():
+    with requests_mock.Mocker() as mocked_requests:
+        mocked_requests.get(
+            client_hackernews.HACKERNEWS_HOST,
+            text='No posts',
+        )
+        with pytest.raises(client_hackernews.HackernewsParseError):
+            client_hackernews.get_posts()
